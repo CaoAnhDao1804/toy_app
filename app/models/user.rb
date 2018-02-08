9fdd0261f6207386e19e84df23fa45bb7ca4835e
CaoAnhDao1804 <caoanhdao18041996@gmail.com>
2018-02-08 16:20:28 +0700

    Edit user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
 	has_many :micriposts
+  validates :name, length: { maximum: 140 },
+          presence: true
+  validates :email, length: { maximum: 140 },
+          presence: true
 
 end
